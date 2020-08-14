e9991401200d6ce837243a275428953eb09818af
github username <github email address>
2020-08-14 15:19:54 -0500

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,27 +1,35 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
+import React from "react";
+import { Card } from "semantic-ui-react";
 
 class PokemonCard extends React.Component {
+  state = {
+    front: true,
+  };
   render() {
+    const poke = this.props.poke;
     return (
       <Card>
-        <div>
+        <div onClick={() => this.setState({ front: !this.state.front })}>
           <div className="image">
-            <img alt="oh no!" />
+            <img
+              src={this.state.front ? poke.sprites.front : poke.sprites.back}
+              style={{ width: 90, height: 90 }}
+              alt="oh no!"
+            />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{poke.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {poke.hp} hp
             </span>
           </div>
         </div>
       </Card>
-    )
+    );
   }
 }
 
-export default PokemonCard
+export default PokemonCard;
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,23 +1,67 @@
-import React from 'react'
-import PokemonCollection from './PokemonCollection'
-import PokemonForm from './PokemonForm'
-import Search from './Search'
-import { Container } from 'semantic-ui-react'
+import React from "react";
+import PokemonCollection from "./PokemonCollection";
+import PokemonForm from "./PokemonForm";
+import Search from "./Search";
+import { Container } from "semantic-ui-react";
 
 class PokemonPage extends React.Component {
+  state = {
+    pokemons: [],
+    search: "",
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/pokemon")
+      .then((res) => res.json())
+      .then((pokemons) => this.setState({ pokemons }));
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const pokemon = {
+      name: e.target.name.value,
+      hp: e.target.hp.value,
+      sprites: {
+        front: e.target.frontUrl.value,
+        back: e.target.backUrl.value,
+      },
+    };
+    e.target.reset();
+    fetch("http://localhost:3000/pokemon", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(pokemon),
+    })
+      .then((res) => res.json())
+      .then((pokemon) => {
+        const pokemons = [...this.state.pokemons, pokemon];
+        this.setState({ pokemons });
+      });
+  };
+
+  handleSearch = (e) => {
+    this.setState({ search: e.target.value });
+  };
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm handleSubmit={this.handleSubmit} />
         <br />
-        <Search />
+        <Search handleSearch={this.handleSearch} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection
+          pokemons={this.state.pokemons}
+          search={this.state.search}
+        />
       </Container>
-    )
+    );
   }
 }
 
-export default PokemonPage
+export default PokemonPage;
